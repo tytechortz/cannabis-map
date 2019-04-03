69b57bafc93552fec3cabd9c5794bff8907a7463
James Swank <jmswank7@gmail.com>
2019-04-03 12:06:47 -0600

    centers map, adds css

--- app.py
+++ app.py
@@ -16,52 +16,66 @@ from dash.dependencies import Input, Output, State
 
 
 
-app = dash.Dash(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 app.config['suppress_callback_exceptions']=True
 
 mapbox_access_token = 'pk.eyJ1IjoidHl0ZWNob3J0eiIsImEiOiJjanN1emtuc2cwMXNhNDNuejdrMnN2aHYyIn0.kY0fOoozCTY-4IUzcLx22w'
 
-
-# df = pd.read_csv(
-#     'https://raw.githubusercontent.com/'
-#     'plotly/datasets/master/'
-#     '1962_2006_walmart_store_openings.csv')
-
 df = gpd.read_file('./cannabis_business.geojson')
 print(df)
 
 #  Layouts
 body = dbc.Container([
-    dbc.Row([
-        html.H1('Colorado Cannabis Businesses'),  
-    ]),
-    dbc.Row([
-         html.Div(id='text-content'),
-    ]),
-    dbc.Row([
-        dcc.Graph(id='map', figure={
-        'data': [{
-            'lat': df['lat'],
-            'lon': df['long'],
-            'marker': {
-                'color': 'blue',
-                'size': 8,
-                'opacity': 0.6
-            },
-            # 'customdata': df['storenum'],
-            'type': 'scattermapbox'
-        }],
-        'layout': {
-            'mapbox': {
-                'accesstoken': 'pk.eyJ1IjoiY2hyaWRkeXAiLCJhIjoiY2ozcGI1MTZ3MDBpcTJ3cXR4b3owdDQwaCJ9.8jpMunbKjdq1anXwU5gxIw'
-            },
-            'hovermode': 'closest',
-            'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0}
-        }
-    })
-    ])
+        dbc.Row([
+            dbc.Col(
+                html.Div(
+                    className='app-header',
+                    children=[
+                        html.Div('COLORADO CANNABIS BUSINESSES', className="app-header--title"),
+                    ]
+                ),
+            ),
+        ]),
+        dbc.Row([
+            html.Div(id='text-content'),
+        ]),
+        dbc.Row([
+            dbc.Col(
+                html.Div([
+                    dcc.Graph(id='map', 
+                        figure={
+                            'data': [{
+                                'lat': df['lat'],
+                                'lon': df['long'],
+                                'marker': {
+                                    'color': 'blue',
+                                    'size': 5,
+                                    'opacity': 0.6
+                                },
+                                # 'customdata': df['storenum'],
+                                'type': 'scattermapbox'
+                            }],
+                            'layout': {
+                                'mapbox': {
+                                    'accesstoken': 'pk.eyJ1IjoiY2hyaWRkeXAiLCJhIjoiY2ozcGI1MTZ3MDBpcTJ3cXR4b3owdDQwaCJ9.8jpMunbKjdq1anXwU5gxIw',
+                                    'center': {
+                                        'lat': 39,
+                                        'lon':-105.5
+                                    },
+                                    'zoom': 6,
+                                },
+                                'hovermode': 'closest',
+                                'height': 550,
+                                'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0}
+                            }
+                        }
+                    ),
+                ]),
+                width={'size':8, 'offset':2 }
+            ),
+        ])
 ])
 
 
