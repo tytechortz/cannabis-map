18535e778e382a09f1fb8968466738ae4d788f68
James Swank <jmswank7@gmail.com>
2019-04-04 20:20:59 -0600

    centers info

--- app.py
+++ app.py
@@ -25,19 +25,15 @@ mapbox_access_token = 'pk.eyJ1IjoidHl0ZWNob3J0eiIsImEiOiJjanN1emtuc2cwMXNhNDNuej
 
 df = gpd.read_file('./cannabis_business.geojson')
 
-# print(df)
+
 color_list = ['purple', 'darkblue', 'dodgerblue', 'darkgreen','black','lightgreen','yellow','orange', 'darkorange','red','darkred','violet']
 
-text = []
-# print(len(df))
 text=[]
 i=0
 while i < len(df):
     text.append(df['Licensee'][i])
     i += 1
-# print(len(text))
 
-# df_MED_Licensed_Transporters = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Transporters'])
 conditions = [
     df['Category'] == 'MED Licensed Transporters',
     df['Category'] == 'MED Licensed Center',
@@ -54,26 +50,13 @@ conditions = [
 ]
 
 df['color'] = np.select(conditions, color_list)
-print(df['color'])
-
-
-
-
-
-
-
 
 
 categories = []
 for i in df['Category'].unique():
     categories.append(i)
 
-
-
-
 colors = dict(zip(categories, color_list))
-
-# print(colors)
 
 #  Layouts
 body = dbc.Container([
@@ -125,34 +108,34 @@ body = dbc.Container([
                 width={'size':10, 'offset':1 }
             ),
         ]),
-        dbc.Row([
-            dbc.Col(
-                dcc.RadioItems(id='types', options=[
-                    {'label':'MED Licensed Transporters','value':'MED Licensed Transporters'},
-                    {'label':'MED Licensed Center','value':'MED Licensed Center'},
-                    {'label':'MED Licensed Cultivator','value':'MED Licensed Cultivator'},
-                    {'label':'MED Licensed Infused Product Manufacturer','value':'MED Licensed Infused Product Manufacturer'},
-                    {'label':'MED Licensed R&D Cultivation','value':'MED Licensed R&D Cultivation'},
-                    {'label':'MED Licensed Retail Operator','value':'MED Licensed Retail Operator'},
-                    {'label':'MED Licensed Testing Facility','value':'MED Licensed Testing Facility'},
-                    {'label':'MED Licensed Retail Marijuana Product Manufacturer','value':'MED Licensed Retail Marijuana Product Manufacturer'},
-                    {'label':'MED Licensed Retail Cultivator','value':'MED Licensed Retail Cultivator'},
-                    {'label':'MED Licensed Retail Testing Facility','value':'MED Licensed Retail Testing Facility'},
-                    {'label':'MED Licensed Retail Transporter','value':'MED Licensed Retail Transporter'},
-                    {'label':'MED Licensed Retail Marijuana Store','value':'MED Licensed Retail Marijuana Store'}
-                    ]),
-                width = {'size': 10}),        
-        ],
-        justify='around',
-        ),
-        dbc.Row([
-            dbc.Col(
-                html.Div(id='table-container'),
-            ),
-            dbc.Col(
-                dcc.Graph(id='map-2'),
-            ),
-        ]),
+        # dbc.Row([
+        #     dbc.Col(
+        #         dcc.RadioItems(id='types', options=[
+        #             {'label':'MED Licensed Transporters','value':'MED Licensed Transporters'},
+        #             {'label':'MED Licensed Center','value':'MED Licensed Center'},
+        #             {'label':'MED Licensed Cultivator','value':'MED Licensed Cultivator'},
+        #             {'label':'MED Licensed Infused Product Manufacturer','value':'MED Licensed Infused Product Manufacturer'},
+        #             {'label':'MED Licensed R&D Cultivation','value':'MED Licensed R&D Cultivation'},
+        #             {'label':'MED Licensed Retail Operator','value':'MED Licensed Retail Operator'},
+        #             {'label':'MED Licensed Testing Facility','value':'MED Licensed Testing Facility'},
+        #             {'label':'MED Licensed Retail Marijuana Product Manufacturer','value':'MED Licensed Retail Marijuana Product Manufacturer'},
+        #             {'label':'MED Licensed Retail Cultivator','value':'MED Licensed Retail Cultivator'},
+        #             {'label':'MED Licensed Retail Testing Facility','value':'MED Licensed Retail Testing Facility'},
+        #             {'label':'MED Licensed Retail Transporter','value':'MED Licensed Retail Transporter'},
+        #             {'label':'MED Licensed Retail Marijuana Store','value':'MED Licensed Retail Marijuana Store'}
+        #             ]),
+        #         width = {'size': 10}),        
+        # ],
+        # justify='around',
+        # ),
+        # dbc.Row([
+        #     dbc.Col(
+        #         html.Div(id='table-container'),
+        #     ),
+        #     dbc.Col(
+        #         dcc.Graph(id='map-2'),
+        #     ),
+        # ]),
 
 
         dbc.Row([
@@ -160,39 +143,50 @@ body = dbc.Container([
                 html.H1("")
         ]),
         dbc.Row([
-            html.Div(id='lic-name'),
+            dbc.Col(
+                html.Div(id='lic-name', style={'height':20, 'text-align': 'center'}),
+            ),
         ]),
         dbc.Row([
-            html.Div(id='biz-name'),
+            dbc.Col(
+                 html.Div(id='biz-name', style={'height':20, 'text-align': 'center'}),
+            ),
         ]),
         dbc.Row([
-            html.Div(id='biz-type'),
+            dbc.Col(
+                html.Div(id='biz-type', style={'height':20, 'text-align': 'center'}),
+            ),
         ]),
         dbc.Row([
-            html.Div(id='city'),
+            dbc.Col(
+                html.Div(id='city', style={'height':20, 'text-align': 'center'}),
+            ),
         ]),
         dbc.Row([
-            html.Div(id='address'),
+            dbc.Col(
+                html.Div(id='address', style={'height':20, 'text-align': 'center'}),
+            ),
         ]),
         dbc.Row([
-            html.Div(id='lic-num'),
+            dbc.Col(
+                html.Div(id='lic-num', style={'height':20, 'text-align': 'center'}),
+            ),
         ]),
         
 ])
-
 
 @app.callback(Output('map-2', 'figure'),
             [Input('types', 'value')])
 def update_figure(selected_type):
     df1 = pd.DataFrame(df.loc[df['Category'] == selected_type])
     data = [dict(
-        lat = df1['lat'],
-        lon = df1['long'],
+        lat = df['lat'],
+        lon = df['long'],
         text = text,
         hoverinfo = 'text',
         type = 'scattermapbox',
-        customdata = df1['uid'],
-        marker = dict(size=6,color='red',opacity=.6)
+        customdata = df['uid'],
+        marker = dict(size=6,color=df['color'],opacity=.6)
     )]
 
     layout = dict(
@@ -209,11 +203,6 @@ def update_figure(selected_type):
 
     fig = dict(data=data, layout=layout)
     return fig
-
-
-
-
-
 
 @app.callback(
     dash.dependencies.Output('lic-name', 'children'),
