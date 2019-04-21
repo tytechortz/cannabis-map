e50e4836fa68c3fe3858bd9daa4140285d863cc7
James Swank <jmswank7@gmail.com>
2019-04-20 21:37:58 -0600

    app4 ready kind of

--- app4.py
+++ app4.py
@@ -16,15 +16,15 @@ app.config['suppress_callback_exceptions']=True
 
 mapbox_access_token = 'pk.eyJ1IjoidHl0ZWNob3J0eiIsImEiOiJjanN1emtuc2cwMXNhNDNuejdrMnN2aHYyIn0.kY0fOoozCTY-4IUzcLx22w'
 
-counties = gpd.read_file('./Colorado_County_Boundaries.geojson')
+counties = gpd.read_file('./Data/Colorado_County_Boundaries.geojson')
 counties.sort_values(by=['US_FIPS'])
 pop_rev = gpd.read_file('./per_cap_joined.geojson')
-# per_rev = pd.read_csv('./revenue_pop_data.csv',header=0, delim_whitespace=False)
+per_rev = pd.read_csv('./revenue_pop_data.csv',header=0, delim_whitespace=False)
 rpd = pop_rev.set_index('COUNTY', drop=False)
-df = gpd.read_file('./cannabis_business.geojson')
+df = gpd.read_file('./Data/cannabis_business.geojson')
 df_revenue = pd.read_csv('https://data.colorado.gov/resource/j7a3-jgd3.csv?$limit=5000&$$app_token=Uwt19jYZWTc9a2UPr7tB6x2k1')
-df_taxes = pd.read_csv('https://data.colorado.gov/resource/3sm5-jtur.csv')
-df_biz = pd.read_csv('https://data.colorado.gov/resource/sqs8-2un5.csv')
+# df_taxes = pd.read_csv('https://data.colorado.gov/resource/3sm5-jtur.csv')
+# df_biz = pd.read_csv('https://data.colorado.gov/resource/sqs8-2un5.csv')
 
 
 df_revenue['county'] = df_revenue['county'].str.upper()
@@ -104,7 +104,8 @@ body = dbc.Container([
             html.Div([
                 daq.BooleanSwitch(
                     id='rev-biz-switch',
-                    on=True
+                    on=True,
+                    color='red'
                 ),
             ]),
             width={'size':1}
@@ -117,21 +118,12 @@ body = dbc.Container([
 
     html.Div(id='rev-stuff'),
     html.Div(id='biz-stuff'),
+    html.Div(id='biz-stuff-2'),
     
     html.Div(id='rev-stuff-2'),
     html.Div(id='rev-stuff-3'),
                   
 ])
-
-# @app.callback(
-#             Output('rev-title', 'children'),
-#             [Input('rev-biz-switch', 'on')])
-# def display_rev_page(switch):
-#     rev_title = []
-#     if switch == True:
-#         rev_title.append(
-#             html.Div('Revenue Data')
-#         )
 
 
 @app.callback(
@@ -154,7 +146,7 @@ def display_rev_page(switch):
                         vertical = True,
                         updatemode = 'drag'
                     ),
-                    width = {'size':1, 'offset':1},
+                    width = {'size':1},
                 ),   
                 dbc.Col(
                     dcc.Graph(id='rev-map',
@@ -162,6 +154,22 @@ def display_rev_page(switch):
                         'scrollZoom': True
                     }),
                     width={'size':8},
+                ),
+                dbc.Col(
+                    html.Div([
+                            html.Table ([
+                                html.Tr([html.Th('Revenue Stats')]),
+                                html.Tr(html.Div(id='county-name', style={'height':30, 'text-align': 'left'})),
+                                # html.Tr(html.Div(id='biz-name', style={'height':30, 'text-align': 'left'})),
+                                # html.Tr(html.Div(id='biz-type', style={'height':30, 'text-align': 'left'})),
+                                # html.Tr(html.Div(id='city', style={'height':30, 'text-align': 'left'})),
+                                # html.Tr(html.Div(id='address', style={'height':30, 'text-align': 'left'})),
+                                # html.Tr(html.Div(id='lic-num', style={'height':30, 'text-align': 'left'})),
+                            ]),
+                        ],
+                        style = {'overflow-x':'scroll'}
+                        ),   
+                    width = {'size':3}
                 ),
                 
                   # style = {'height': 50}
@@ -179,51 +187,70 @@ def display_biz_page(switch):
         biz_page.append(
             dbc.Row([
                 dbc.Col(
-                    html.Div([
-                        html.Div(html.Button('All License Types',id='button-all', n_clicks=0)),
-                        html.Div(html.Button('Transporters', id='button-transporters', n_clicks=0)),
-                        html.Div(html.Button('Center', id='button-center', n_clicks=0)),
-                        html.Div(html.Button('Cultivator', id='button-cultivator', n_clicks=0)),
-                        html.Div(html.Button('Infused Product Manufacturer', id='button-ipm', n_clicks=0)),
-                        html.Div(html.Button('R&D Cultivation', id='button-rdc', n_clicks=0)),
-                        html.Div(html.Button('Retail Operator', id='button-operator', n_clicks=0)),
-                        html.Div(html.Button('Testing Facility', id='button-testing', n_clicks=0)),
-                        html.Div(html.Button('Retail Marijuana Product Manufacturer', id='button-rmpm', n_clicks=0)),
-                        html.Div(html.Button('Retail Cultivator', id='button-ret-cult', n_clicks=0)),
-                        html.Div(html.Button('Retail Testing Facility', id='button-ret-test', n_clicks=0)),
-                        html.Div(html.Button('Retail Transporter', id='button-ret-trans', n_clicks=0)),
-                        html.Div(html.Button('Retail Marijuana Store', id='button-ret-store', n_clicks=0)),
-                    ]),
-                    width = {'size':2}
-                ),
-                dbc.Col(
                     dcc.Graph(id='biz-map',
                     config={
                         'scrollZoom': True
                     }),
-                    width={'size':6},
+                    width={'size':8},
                 ),
                 dbc.Col(
-                        html.Div([
-                            html.Table ([
-                                html.Tr([html.Th('Business Info')]),
-                                html.Tr(html.Div(id='lic-name', style={'height':30, 'text-align': 'left'})),
-                                html.Tr(html.Div(id='biz-name', style={'height':30, 'text-align': 'left'})),
-                                html.Tr(html.Div(id='biz-type', style={'height':30, 'text-align': 'left'})),
-                                html.Tr(html.Div(id='city', style={'height':30, 'text-align': 'left'})),
-                                html.Tr(html.Div(id='address', style={'height':30, 'text-align': 'left'})),
-                                html.Tr(html.Div(id='lic-num', style={'height':30, 'text-align': 'left'})),
-                            ]),
-                        ],
-                        style = {'overflow-x':'scroll'}
-                        ),   
-                    width = {'size':4}
+                html.Div(
+                    className='radio',
+                    children=[ 
+                    dcc.RadioItems(id='categories', options=[
+                        {'label':'', 'value':'all'},
+                        {'label':'','value':'MED Licensed Transporters'},
+                        {'label':'','value':'MED Licensed Center'},
+                        {'label':'','value':'MED Licensed Cultivator'},
+                        {'label':'','value':'MED Licensed Infused Product Manufacturer'},
+                        {'label':'','value':'MED Licensed R&D Cultivation'},
+                        {'label':'','value':'MED Licensed Retail Operator'},
+                        {'label':'','value':'MED Licensed Testing Facility'},
+                        {'label':'','value':'MED Licensed Retail Marijuana Product Manufacturer'},
+                        {'label':'','value':'MED Licensed Retail Cultivator'},
+                        {'label':'','value':'MED Licensed Retail Testing Facility'},
+                        {'label':'','value':'MED Licensed Retail Transporter'},
+                        {'label':'','value':'MED Licensed Retail Marijuana Store'},
+                    ],
+                    labelStyle={'display':'block', 'margin': 0, 'padding': 1},
+                    value = 'all'
+                    ),
+                ]),
+                width = {'size':.1}
+                ),
+                dbc.Col(
+                html.Div([
+                    html.Table ([
+                        html.Tr(html.Div('All License Types', id='lics-num9', style={'font-size':17.5})),
+                        html.Tr(html.Div('Transporters', id='lics-num10', style={'font-size':17.5})),
+                        html.Tr(html.Div('Center', id='lics-num11', style={'font-size':17.5})),
+                        html.Tr(html.Div('Cultivator', id='lics-num12', style={'font-size':17.5})),
+                        html.Tr(html.Div('Infused Product Mfg.', id='lics-num13', style={'font-size':17.5})),
+                        html.Tr(html.Div('R&D Cultivation', id='lics-num1', style={'height':26,'font-size':17.5})),
+                        html.Tr(html.Div('Retail Operator', id='lics-num2', style={'height':26,'font-size':17.5})),
+                        html.Tr(html.Div('Testing Facility', id='lics-num3', style={'height':26,'font-size':17.5})),
+                        html.Tr(html.Div('Retail Marijuana Product Mfg.', id='lics-num4', style={'height':26,'font-size':17.5})),
+                        html.Tr(html.Div('Retail Cultivator', id='lics-num5', style={'height':26,'font-size':17.5})),
+                        html.Tr(html.Div('Retail Testing Facility', id='lics-num6', style={'height':26,'font-size':17.5})),
+                        html.Tr(html.Div('Retail Transporter', id='lics-num7', style={'height':26,'font-size':17.5})),
+                        html.Tr(html.Div('Retail Marijuana Store', id='lics-num8', style={'font-size':17.5})),
+                        html.Tr([html.Th('Business Info',style={'height':50, 'text-align': 'center'})]),
+                        html.Tr(html.Div(id='lic-name', style={'height':50, 'text-align': 'left'})),
+                        html.Tr(html.Div(id='biz-name', style={'height':50, 'text-align': 'left'})),
+                        html.Tr(html.Div(id='biz-type', style={'height':30, 'text-align': 'left'})),
+                        html.Tr(html.Div(id='city', style={'height':30, 'text-align': 'left'})),
+                        html.Tr(html.Div(id='address', style={'height':30, 'text-align': 'left'})),
+                        html.Tr(html.Div(id='lic-num', style={'height':30, 'text-align': 'left'})),
+                    ]),
+                ],
+                style = {'overflow-x':'scroll'}
+                ),
+                width = {'size':3}
                 ),
             ]),
         ) 
        
         return biz_page
-
 
 
 @app.callback(
@@ -423,188 +450,57 @@ def create_rev_bar(clickData,switch):
 @app.callback(
             Output('biz-map', 'figure'),
             [Input('rev-biz-switch', 'on'),
-            Input('button-all', 'n_clicks'),
-            Input('button-transporters','n_clicks'),
-            Input('button-center','n_clicks'),
-            Input('button-cultivator','n_clicks'),
-            Input('button-ipm','n_clicks'),
-            Input('button-rdc','n_clicks'),
-            Input('button-operator','n_clicks'),
-            Input('button-testing','n_clicks'),
-            Input('button-rmpm','n_clicks'),
-            Input('button-ret-cult','n_clicks'),
-            Input('button-ret-test','n_clicks'),
-            Input('button-ret-trans','n_clicks'),
-            Input('button-ret-store','n_clicks'),])
-def update_figure_a(switch,all_clicks,trans_clicks,center_clicks,cultivator_clicks,
-ipm_clicks,rdc_clicks,operator_clicks,testing_clicks,rmpm_clicks,ret_cult_clicks,ret_test_clicks,ret_trans_clicks,ret_store_clicks):
+            Input('categories', 'value')])
+def update_figure_a(switch,selected_values):
     
     rpd_s = rpd.sort_values(by=['RId2'])
   
     rpd_s = rpd_s.apply(pd.to_numeric, errors='ignore')
     rpd_s = rpd_s.fillna(0)
 
-    counties_s = counties.sort_values(by=['US_FIPS'])
+    # counties_s = counties.sort_values(by=['US_FIPS'])
   
 
     data = [dict(
             type = 'scattermapbox',
         )]
-    if all_clicks % 2 == 1:
-        data = [dict(
-            lat = df['lat'],
-            lon = df['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df['uid'],
-            marker = dict(size=10,color=df['color'],opacity=.6)
+
+    df1 = pd.DataFrame(df.loc[df['Category'] == selected_values])
+    if selected_values == 'all':
+            filtered_df = df
+            data = [dict(
+                lat = df['lat'],
+                lon = df['long'],
+                text = text,
+                hoverinfo = 'text',
+                type = 'scattermapbox',
+                customdata = df['uid'],
+                marker = dict(size=10,color=df['color'],opacity=.6)
             )]
-           
-    elif trans_clicks % 2 == 1:
-        df1 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Transporters'])
-        data = [dict(
-            lat = df1['lat'],
-            lon = df1['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df['uid'],
-            marker = dict(size=10,color=df1['color'],opacity=.6)
-        )]
-    elif center_clicks % 2 == 1:
-        df2 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Center'])
-        data = [dict(
-            lat = df2['lat'],
-            lon = df2['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df['uid'],
-            marker = dict(size=10,color=df2['color'],opacity=.6)
-        )]
-    elif cultivator_clicks % 2 == 1:
-        df3 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Cultivator'])
-        data = [dict(
-            lat = df3['lat'],
-            lon = df3['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df['uid'],
-            marker = dict(size=10,color=df3['color'],opacity=.6)
-        )]
-    elif ipm_clicks % 2 == 1:
-        df4 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Infused Product Manufacturer'])
-        data = [dict(
-            lat = df4['lat'],
-            lon = df4['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df4['uid'],
-            marker = dict(size=10,color=df4['color'],opacity=.6)
-        )]
-    elif rdc_clicks % 2 == 1:
-        df5 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed R&D Cultivation'])
-        data = [dict(
-            lat = df5['lat'],
-            lon = df5['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df5['uid'],
-            marker = dict(size=10,color=df5['color'],opacity=.6)
-        )]
-    elif operator_clicks % 2 == 1:
-        df6 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Retail Operator'])
-        data = [dict(
-            lat = df6['lat'],
-            lon = df6['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df6['uid'],
-            marker = dict(size=10,color=df6['color'],opacity=.6)
-        )]
-    elif testing_clicks % 2 == 1:
-        df7 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Testing Facility'])
-        data = [dict(
-            lat = df7['lat'],
-            lon = df7['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df7['uid'],
-            marker = dict(size=10,color=df7['color'],opacity=.6)
-        )]
-    elif rmpm_clicks % 2 == 1:
-        df8 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Retail Marijuana Product Manufacturer'])
-        data = [dict(
-            lat = df8['lat'],
-            lon = df8['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df8['uid'],
-            marker = dict(size=10,color=df8['color'],opacity=.6)
-        )]
-    elif ret_cult_clicks % 2 == 1:
-        df9 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Retail Cultivator'])
-        data = [dict(
-            lat = df9['lat'],
-            lon = df9['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df9['uid'],
-            marker = dict(size=10,color=df9['color'],opacity=.6)
-        )]
-    elif ret_test_clicks % 2 == 1:
-        df10 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Retail Testing Facility'])
-        data = [dict(
-            lat = df10['lat'],
-            lon = df10['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df10['uid'],
-            marker = dict(size=10,color=df10['color'],opacity=.6)
-        )]
-    elif ret_trans_clicks % 2 == 1:
-        df11 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Retail Transporter'])
-        data = [dict(
-            lat = df11['lat'],
-            lon = df11['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df11['uid'],
-            marker = dict(size=10,color=df11['color'],opacity=.6)
-        )]
-    elif ret_store_clicks % 2 == 1:
-        df12 = pd.DataFrame(df.loc[df['Category'] == 'MED Licensed Retail Marijuana Store'])
-        data = [dict(
-            lat = df12['lat'],
-            lon = df12['long'],
-            text = text,
-            hoverinfo = 'text',
-            type = 'scattermapbox',
-            customdata = df12['uid'],
-            marker = dict(size=10,color=df12['color'],opacity=.6)
-        )]
+    else: 
+            filtered_df = df1
+            data = [dict(
+                lat = filtered_df['lat'],
+                lon = filtered_df['long'],
+                text = text,
+                hoverinfo = 'text',
+                type = 'scattermapbox',
+                customdata = df1['uid'],
+                marker = dict(size=7,color=df1['color'],opacity=.6)
+            )]
+    
     layout = dict(
-        mapbox = dict(
-            accesstoken = mapbox_access_token,
-            center = dict(lat=39, lon=-105.5),
-            zoom = 6.25,
-            style = 'light'
-        ),
-        hovermode = 'closest',
-        height = 600,
-        margin = dict(r=0, l=0, t=0, b=0),
-        clickmode = 'event+select'
-    )    
+            mapbox = dict(
+                accesstoken = mapbox_access_token,
+                center = dict(lat=39, lon=-105.5),
+                zoom = 6.5,
+                style = 'light'
+            ),
+            hovermode = 'closest',
+            height = 700,
+            margin = dict(r=0, l=0, t=0, b=0),
+            clickmode = 'event+select'
+        )  
   
         
     fig = dict(data=data, layout=layout)
